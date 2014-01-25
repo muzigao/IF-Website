e8ab22db5507ed290f342a260e92b8b8621c4da1
esantenna <cz578@nyu.edu>
2014-01-24 21:07:50 -0500

    Fix a bug on twitter login after FB login added

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,9 @@ def self.from_omniauth(auth)
     user.uid = auth.uid
     user.name = auth.info.name
     user.oauth_token = auth.credentials.token
-    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+    if auth.credentials.expires_at
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+    end
     user.save!
   end
 end
